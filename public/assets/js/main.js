98abdfdb9b970b63d9454ab90d618ceb8fa45b18
Juan Díaz <uaujuan128@gmail.com>
2021-09-17 14:36:49 +0200

    Cambios de estructura y diseño

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,7 @@
 //Set footer fixed in the bottom of the page when body height is smaller than window height
 $( document ).ready(function() {
     if ($('html').height() <= $(window).height()) {
-        $('.footer').css('marginTop', $(document).innerHeight() - $('main').innerHeight() - $('header').innerHeight() - $('.footer').innerHeight());
+        $('footer').css('marginTop', $(document).innerHeight() - $('main').innerHeight() - $('header').innerHeight() - $('footer').innerHeight());
     }
 });
 
